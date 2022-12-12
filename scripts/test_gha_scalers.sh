48aed777e1327c8b4c2be60eaca0baf389acc45d
Chris Fowlkes <cpfowlke@gmail.com>
2022-12-11 19:57:20 -0500

    add queued one

--- scripts/test_gha_scalers.sh
+++ scripts/test_gha_scalers.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # generate 10 gha workflow runs to trigger auto scaling
-for i in {1..10}
-do
-    git commit --allow-empty -m "bump"
-    git push
-done
+git commit --allow-empty -m "bump"
+git push
